3696cf9a9d406cbb5a1cf8e406886499eca95d5c
Toei <itthisakp@gmail.com>
2021-01-20 16:15:31 +0700

    andd interface and fix typo

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+
+export declare const callback: (res: object) => void
+
+export function presentIndoorMap(): void;
+export function getAllStores(callback?: callback): void;
+export function setOrigin(store_id?: string): void;
+export function setDestination(store_id?: string): void;
+export function getStore(store_id?: string, callback?: callback): void;
+export function setStores(stores?: object): void;
+export function getVenues(successCallback?: callback, errorCallback?: callback): void;
--- ios/RNIndoormap.podspec
+++ ios/RNIndoormap.podspec
@@ -12,7 +12,7 @@ Pod::Spec.new do |s|
   s.platform     = :ios, "10.0"
   s.ios.deployment_target = "10.0"
   s.source       = { :git => "https://github.com/author/RNIndoormap.git", :tag => "master" }
-  s.source_files  = "RNIndoormap/**/*.{h,m}"
+  s.source_files  = "*.{h,m}"
   s.requires_arc = true
 
 
